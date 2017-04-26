d9e218f66c8c519044efa93c5bc7dee312906ed9
MA.Cherifi <MA.Cherifi@790-444-29.interne.college-gerald-godin.qc.ca>
2017-04-26 16:51:20 -0400

    maj fonctionspecifique

--- fonctions-specifiques-projet-final.php
+++ fonctions-specifiques-projet-final.php
@@ -2,19 +2,24 @@
 
 function creeTableUtilisateurs($oBD,$strNomTableUtilisateurs){
     $strDefinitions = "N,NoUtilisateur;" . "V50,Courriel;" . "V50,MotDePasse;" . "D,Creation;" . 
-            "E,NbConnexions;" . "E,Status;" . "E,NoEmpl;". "V25,Nom;" . "V20,Prenom;" . 
+            "E,NbConnexions;" . "E,Status;" . "N,NoEmpl;". "V25,Nom;" . "V20,Prenom;" . 
             "V16,NoTelMaison;" . "V22,NoTelTravail;" . "V16,NoTelCellulaire;" . "D,Modification;" . "V50,AutresInfos";
     $strCles = "NoUtilisateur";
     
-    $oBD->creeTableGenerique($strNomTableTypesLivraison, $strDefinitions, $strCles);
+    $oBD->creeTableGenerique($strNomTableUtilisateurs, $strDefinitions, $strCles);
 }
 
 function creeTableConnexions($oBD,$strNomTableConnexions){
-    $strDefinitions = "E,NoConnexion;" . "E,NoUtilisateurs;" . "V50,MotDePasse;" . "D,Creation;" . 
-            "E,NbConnexions;" . "E,Status;" . "E,NoEmpl;". "V25,Nom;" . "V20,Prenom;" . 
-            "V16,NoTelMaison;" . "V22,NoTelTravail;" . "V16,NoTelCellulaire;" . "D,Modification;" . "V50,AutresInfos";
-    $strCles = "NoUtilisateur";
+    $strDefinitions = "N,NoConnexion;" . "N,NoUtilisateurs;" . "D,Connexion;" . "D,Deconnexion";
+    $strCles = "NoConnexion";
     
-    $oBD->creeTableGenerique($strNomTableTypesLivraison, $strDefinitions, $strCles);
+    $oBD->creeTableGenerique($strNomTableConnexions, $strDefinitions, $strCles);
+}
+
+function creeTableAnnonces($oBD,$strNomTableAnnonces){
+    $strDefinitions = "N,NoAnnonce;" . "N,NoUtilisateurs;" . "D,Parution;" . "N,Categorie;" ;
+    $strCles = "NoConnexion";
+    
+    $oBD->creeTableGenerique($strNomTableAnnonces, $strDefinitions, $strCles);
 }
 ?>
