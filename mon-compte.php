709e992f48302b59b7ecb869129e711809a16f72
Alain Abboud <abboudalain@gmail.com>
2017-04-26 22:27:59 -0400

    verifications user
    asdasd

--- mon-compte.php
+++ mon-compte.php
@@ -1,4 +1,49 @@
 <!doctype html>
+<?php
+
+    require_once 'classe-mysql-2017-04-26.php';
+    require_once 'classe-fichier-2017-04-26.php';
+    require_once 'librairies-communes-2017-04-07.php';
+    
+    $strMethode = get('MAJ');
+    
+    $strStatut = get('Statut');
+    $intNoEmpl = get('NoEmpl');
+    $strPrenom = get('Prenom');
+    $strNomFamille = get('NomFamille');
+    $strCourriel = get('Courriel');
+    $strMotPasse = get('MotPasse');
+    
+    if(isset($strMethode)){
+        if($strStatut==""){
+            echo '<script language="javascript">';
+            echo 'alert("Statut non valide")';
+            echo '</script>';
+        }
+        else if(!estNumerique($intNoEmpl)){
+            echo '<script language="javascript">';
+            echo "alert('Numéro d''employé invalide.')";
+            echo '</script>';
+        }
+        else if(dansIntervalle($intNoEmpl, 1, 9999)){
+            echo '<script language="javascript">';
+            echo "alert('Numéro d'employé hors plage (1-9999)";
+            echo '</script>';
+        }
+        else if(strlen($strPrenom) == 0){
+            echo '<script language="javascript">';
+            echo "alert('Prénom absent')";
+            echo '</script>';
+        }
+        else if(strlen($strNomFamille) == 0){
+            echo '<script language="javascript">';
+            echo "alert('Nom de famille absent')";
+            echo '</script>';
+        }
+    }
+   
+?>
+
 <html class="no-js" lang="">
     <head>
         <meta charset="utf-8">
@@ -33,14 +78,14 @@
 
       <div id="content3">
 
-        <form>
+        <form id="frmSaisie"  method="get" action="" >
                <table width="80%" cellpadding="100" style="align: center;">
                    <caption style="font-weight: bold;font-variant: small-caps;font-size: 150%;height:40px;line-height: 40px; margin-bottom: 5px;" ><a>Mise à jour de vos informations</a></caption>
                    <tr>
                        <td style="font-weight: bold;font-size: 110%">Statut d'employé</td>
                        <td>
 
-                           <select class="annonces">
+                           <select class="annonces" id="Statut" name="Statut">
                              <option disabled selected hidden>Statut</option>
                              <option name="EtatActif" value="1">Actif</option>
                              <option name="EtatInactif" value="0">Inactif</option>
@@ -50,51 +95,51 @@
                    <tr>
                        <td style="font-weight: bold;font-size: 110%">Numéro d'employé</td>
                        <td>
-                           <input class="sInput" type="text" name="NoEmpl">
+                           <input class="sInput" id="NoEmpl" type="text" name="NoEmpl">
                        </td>
                    </tr>
                    <tr>
                        <td style="font-weight: bold;font-size: 110%">Prénom</td>
                        <td>
-                           <input class="sInput"  type="text" name="Prenom" >
+                           <input class="sInput" id="Prenom" type="text" name="Prenom" >
                        </td>
                    </tr>
                    <tr>
                        <td style="font-weight: bold;font-size: 110%">Nom de famille</td>
                        <td>
-                           <input class="sInput"  type="text" name="NomFamille" >
+                           <input class="sInput" id="NomFamille" type="text" name="NomFamille" >
                        </td>
                    </tr>
                    <tr>
                        <td style="font-weight: bold;font-size: 110%">Courriel</td>
                        <td>
-                           <input class="sInput"  type="email" name="Courriel" value="alainabboud@bell.net" disabled="">
+                           <input class="sInput" id="Courriel" type="email" name="Courriel" value="alainabboud@bell.net" disabled="">
                        </td>
                    </tr>
                    <tr>
                        <td style="font-weight: bold;font-size: 110%">Mot de passe</td>
                        <td>
-                           <input class="sInput"  type="password" name="motPasse" value="6249613"    disabled="">
+                           <input class="sInput" id="motPasse" type="password" name="motPasse" value="6249613" disabled="">
                        </td>
 
                    </tr>
                    <tr>
                        <td style="font-weight: bold;font-size: 110%"></td>
                        <td>
-                           <input class="sInput" type="checkbox" name="public" min="1" step="any" ><label>Rendre mes informations publiques</label>
+                           <input class="sInput" type="checkbox" id="public" name="public" min="1" step="any" ><label>Rendre mes informations publiques</label>
+                       </td>
+                   </tr>
+                   <tr>
+                       <td style="font-weight: bold;font-size: 110%"></td>
+                       <td>
+                           <button class="btn" type="submit" value="MAJ" >Mise à jour</button>
                        </td>
                    </tr>
                </table>
            </form>
 
 
-        <div id="admin2">
-            <button class="btn" >Mise à jour</button>
-        </div>
-
-
-
-
+        
 
        </main>
 
